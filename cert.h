b8df8f52781f86424bcfd7edd7d97045942a0c7c
Sebastian <sebastian.krahmer@gmail.com>
2018-04-19 14:34:26 +0200

    - psc: adding pinned certs

---
+++ cert.h
@@ -0,0 +1,12 @@
+#ifndef psc_cert_h
+#define psc_cert_h
+
+namespace ns_psc {
+
+#include "x509.h"
+
+}
+
+#endif
+
+
---
+++ x509.h
@@ -0,0 +1,10 @@
+unsigned char the_subject_name[1] = {
+	0x00
+};
+unsigned char the_public_key[1] = {
+	0x00
+};
+unsigned char the_certificate[1] = {
+	0x00
+};
+
